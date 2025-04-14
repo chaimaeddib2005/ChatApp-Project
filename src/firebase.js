aa27d7e02cec6480e0a345197c868809870c5207
kimad-cy <khadija.imad@um6p.ma>
2025-04-14 11:19:55 +0100

    firebase inclut

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzzronBZr1aQ2nxeJiq8_jHjOPYpWsKXE",
+  authDomain: "chatapp-a9297.firebaseapp.com",
+  databaseURL: "https://chatapp-a9297-default-rtdb.firebaseio.com",
+  projectId: "chatapp-a9297",
+  storageBucket: "chatapp-a9297.firebasestorage.app",
+  messagingSenderId: "806594827553",
+  appId: "1:806594827553:web:786e07011753d1b014da60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
